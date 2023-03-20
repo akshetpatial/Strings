556d13e6bb24c50eb9fbcb42c45b3e3e7df96094
AKSHET PATIAL <akshet.patial@gmail.com>
2023-03-21 01:48:37 +0530

    Created the Palindrome.java file

---
+++ src/Palindrome.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Palindrome {
+
+    public static boolean isPalindrome(String input){
+
+        int start=0;
+        int end = input.length()-1;
+
+        while(start<=end){
+            if(input.charAt(start)!=input.charAt(end))
+                return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter any String: ");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+
+        boolean check = isPalindrome(input);
+        if(check)
+            System.out.println(input+ " is a Palindrome");
+        else
+            System.out.println(input+ " is not a Palindrome");
+
+    }
+}
