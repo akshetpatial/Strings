9b09e1443649630572613a448a49526194304ed4
AKSHET PATIAL <akshet.patial@gmail.com>
2023-03-21 00:41:08 +0530

    Created the StringClassFile.java and made some minor changes

---
+++ src/StringClass.java
@@ -0,0 +1,14 @@
+public class StringClass {
+
+    public static void main(String[] args) {
+
+        String name= "akshet";
+        System.out.println("my name is: "+name);
+
+        float f = 123456.654f;
+        double d = 2432564.348334;
+        System.out.println("Rond Off the Number: "+ f);
+        System.out.printf("Round off the number %.2f and print my name as well %s",d,name);
+    }
+
+}
